ed149434099f8729a78339ec2c3b3bfebb27275d
DEV <32570077+sobadev@users.noreply.github.com>
2019-04-04 10:39:02 +0530

    Update info_page.cpp

--- src/info_page.cpp
+++ src/info_page.cpp
@@ -195,7 +195,7 @@ void InfoPage::refreshInfoWidget()
 
         int pos = 0;
         for (auto key : m_info_params.keys()) {
-		if ((key != "Model") && (m_info_params.value(key) != "Vendor")) {
+		if ((key != "Model") && (key != "Vendor")) {
                          
                 QLabel *valueLbl = new QLabel(this);
                 QLabel *keyLbl = new QLabel(this);
@@ -216,7 +216,7 @@ void InfoPage::refreshInfoWidget()
                 ui->paramLayout->addWidget(new QLabel("Advanced"), pos, 0, 1, 1);
                 pos++;
                 for (auto key : m_info_params_advanced.keys()) {
-			if( (key != "Model") && (m_info_params.value(key) != "Vendor")) {
+			if( (key != "Model") && (key != "Vendor")) {
                         QLabel *valueLbl = new QLabel(this);
                         QLabel *keyLbl = new QLabel(this);
                         valueLbl->setText(m_info_params_advanced.value(key));
